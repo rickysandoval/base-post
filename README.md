73020473de27cc4c81702cb6f1e2524f82a167ed
Ricky Sandoval <rsandoval@arrow.com>
2015-12-08 14:26:30 -0700

    Intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# BaseTheme
+
+This is a base theme for projects, uses Cole Peters' Gemma: 
+
+https://github.com/colepeters/gemma
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+// NPM modules
+var gulp = require('gulp')
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var cssnext = require('cssnext')
+var postcss = require('gulp-postcss')
+var stylelint = require('stylelint')
+var reporter = require('postcss-reporter')
+
+gulp.task('serve', ['compileCss'], function() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'StyleGuide.html'
+    }
+  });
+
+  gulp.watch('styles/src/**/*.css', ['compileCss']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// gulp.task('csslint', function () {
+//   return gulp.src('./src/**/*.css')
+//   .pipe(postcss([
+//     require('precss'),
+//     stylelint({
+//       'rules': config.rules
+//     }),
+//     reporter({
+//       clearMessages: true
+//     })
+//   ]))
+// })
+
+gulp.task('compileCss', function() {
+  return gulp.src('./styles/src/main.scss')
+    .pipe(postcss([
+      require('precss')({}),
+      cssnext({
+        compress: true
+      })
+    ]))
+    .pipe(gulp.dest('./styles/dist'));
+    //.pipe(browserSync.stream());
+});
+
+gulp.task('compileAndMinCss', function() {
+  return gulp.src('./styles/src/main.scss')
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./styles/dist'));
+});
