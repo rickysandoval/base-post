d61e58e9f0fe324484513bac7f2bd9d40c277216
Ricky Sandoval <rsandoval@arrow.com>
2016-02-23 00:27:57 -0700

    Get demo up and start settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,50 @@
 // NPM modules
-var gulp = require('gulp')
-var rename = require('gulp-rename');
-var watch = require('gulp-watch');
-var browserSync = require('browser-sync').create();
-var cssnext = require('postcss-cssnext');
-var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
+const gulp = require('gulp')
+const rename = require('gulp-rename');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+const cssnext = require('postcss-cssnext');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const del = require('del');
 
+gulp.task('clean', function(){
+  del(['styles/dist']);
+});
 
-gulp.task('serve', ['buildCss'], function() {
+gulp.task('serve', ['dev'], function() {
   browserSync.init({
     server: {
       baseDir: './',
-      index: 'StyleGuide.html'
+      index: 'demo.html'
     }
   });
 
-  gulp.watch('styles/src/**/*.css', ['buildCss']);
+  gulp.watch(['styles/src/**/*.css', '_demo.css'], ['dev']);
   gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task('buildCss', function() {
+gulp.task('dev', function() {
+  return gulp.src('./_demo.css')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      require('precss')({}),
+      require('autoprefixer'),
+      cssnext({
+        compress: true,
+        sourcemap: true
+      })
+    ]))
+    .on('error', function(e){ console.log(e.message); this.emit('end'); })
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', function() {
   return gulp.src('./styles/src/main.css')
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -38,3 +61,4 @@ gulp.task('buildCss', function() {
     .pipe(gulp.dest('./styles/dist'))
     .pipe(browserSync.stream());
 });
+
