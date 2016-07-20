276552ab501b3451f391766572c6367c6f005c42
Ricky Sandoval <rsandoval@arrow.com>
2016-07-20 16:11:15 -0600

    Work on toggle positions and add size change demo to form

--- demo-src/app/components.js
+++ demo-src/app/components.js
@@ -1,3 +1,19 @@
 angular.module('demoApp').component('components', {
-	templateUrl: 'components.html'
+	templateUrl: 'components.html',
+	controller: function() {
+		this.changeFormSize = function(fontSize) {
+			switch (fontSize) {
+				case 'small':
+					this.sizeClass="fontSize-n1"
+					break;
+				case 'normal':
+				this.sizeClass="fontSize-1"
+					break;
+				case 'large':
+					this.sizeClass="fontSize-2"
+					break
+			}
+		}
+		this.sizeClass = 'fontSize-1';
+	}
 });
